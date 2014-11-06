56a784f6f8f0da45b9510b1b0f0ea6032a873d1d
LuminousElement <d.tullemans@gmail.com>
2014-11-06 18:22:25 +1000

    Demo code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
-//#include "FastLED/FastLED.h"
+#include "FastLED/FastLED.h"
+
+#define NUM_LEDS 8
+CRGB buffer[NUM_LEDS];
 
 int main() {
-  
+  FastLED::addLeds<NEOPIXEL, 10>(buffer, NUM_LEDS);
   return 0;
 }
